b296736b2d35e3670efec5899586018575c810b7
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-07 17:34:37 +0900

    83. Implementing the First Navigator

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
@@ -15,7 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // NOTE: Delete this line at `83. Implementing the First Navigator` to show the content.
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
