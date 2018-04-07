5fbad804da2049db51de6cdb945f6b534e0f9547
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-07 17:59:15 +0900

    87. Customization with Header Options

--- App.js
+++ App.js
@@ -4,12 +4,28 @@ import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
       auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
     });
 
     return (
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ReviewScreen extends Component {
+  // NOTE: class property
+  static navigationOptions = {
+    title: 'Review Jobs',
+    // NOTE: Rewrite as follows at `87. Customization with Header Options`.
+    headerRight: <Text>Right</Text>
+  }
+
   render() {
     return (
       <View>
