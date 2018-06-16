a6640ebb401d46cd633aa6cc7a2d8c45d4c4c011
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-06-16 16:16:05 +0900

    Put tabs at the bottom on Android with react-navigation v2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Platform, View, StyleSheet } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Constants } from 'expo';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -9,25 +10,52 @@ import DeckScreen from './screens/DeckScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ReviewScreen from './screens/ReviewScreen';
 
+// NOTE: The following implmentation is based on react-navigation v2.3.0 while the udemy course is based on v1.x.x.
+//   See: Q&A on https://www.udemy.com/react-native-advanced/learn/v4/t/lecture/6853588 (subject: App.js without TabNavigator)
+//
+// `styles` should be defined before being used in this case.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ...Platform.select({
+      android: {
+        marginTop: Constants.statusBarHeight
+      }
+    })
+  },
+  stackHeaderStyle: {
+    ...Platform.select({
+      android: {
+        marginTop: -Constants.statusBarHeight
+      }
+    })
+  }
+});
+
+const ReviewNavigator = createStackNavigator(
+  {
+    review: ReviewScreen,
+    settings: SettingsScreen
+  },
+  {
+    navigationOptions: {
+      headerStyle: styles.stackHeaderStyle
+    }
+  }
+);
+
+const MainNavigator = createBottomTabNavigator({
+  welcome: WelcomeScreen,
+  auth: AuthScreen,
+  main: createBottomTabNavigator({
+    map: MapScreen,
+    deck: DeckScreen,
+    review: ReviewNavigator
+  })
+});
+
 export default class App extends React.Component {
   render() {
-    const MainNavigator = TabNavigator({
-      welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen },
-      main: {
-        screen: TabNavigator({
-          map: { screen: MapScreen },
-          deck: { screen: DeckScreen },
-          review: {
-            screen: StackNavigator({
-              review: { screen: ReviewScreen },
-              settings: { screen: SettingsScreen }
-            })
-          }
-        })
-      },
-    });
-
     return (
       <View style={styles.container}>
         <MainNavigator />
@@ -35,13 +63,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // NOTE: Delete this line at `83. Implementing the First Navigator` to show the content.
-    // alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
