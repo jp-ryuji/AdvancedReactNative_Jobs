be68563d4322bb88b545c7a2d871e3f29109493f
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-06-16 18:07:47 +0900

    Change Button style in accordance with react-native-elements 1.0.0-beta5

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -6,14 +6,15 @@ class ReviewScreen extends Component {
   // NOTE: This is a class property, that can't access props.
   // NOTE: Rewrite as follows at `87. Customization with Header Options` and `88. Programmatic Navigation`.
   //   You should check out QA on each video.
+  // NOTE: Additionally, the following was rewritten for react-native-elements 1.0.0-beta5.
   static navigationOptions = ({ navigation }) => ({
     title: 'Review Jobs',
     headerRight: (
       <Button
         title='Settings'
         onPress={() => navigation.navigate('settings')}
-        backgroundColor="rgba(0,0,0,0)"
-        color="rgba(0, 122, 255, 1)" // Blue
+        clear
+        titleStyle={{ color: "rgba(0, 122, 255, 1)" }}
       />
     )
   });
