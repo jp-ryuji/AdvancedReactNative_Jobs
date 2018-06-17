3713c0aa26bdaa5dd39264bc94f207dd07db8037
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-06-17 18:49:02 +0900

    113 ~ 117. Issuing Action Creators from MapScreen

---
+++ actions/job_actions.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
+import { INDEED_PUBLISHER_ID } from 'react-native-dotenv';
+
+import {
+  FETCH_JOBS,
+} from './types';
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_QUERY_PARAMS = {
+  publisher: INDEED_PUBLISHER_ID,
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript' // NOTE: This is hard-coded for now. But it should be entered by a user.
+};
+
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
+// NOTE: `(dispatch)` can be just `dispatch`
+export const fetchJobs = (region, callback) => async (dispatch) => {
+  try {
+    let zip = await reverseGeocode(region); // NOTE: reverseGeocode accepts { latitude, longitude }, that are included in `region`
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
+  } catch(e) {
+    console.error(e);
+  }
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -22,6 +26,12 @@ class MapScreen extends Component {
     this.setState({ region });
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck');
+    });
+  }
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -38,9 +48,28 @@ class MapScreen extends Component {
           style={{ flex: 1 }}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+// NOTE: `actions` can be `{ fetchJobs }`. Then you should change `import * as actions from '../actions';`, too.
+export default connect(null, actions)(MapScreen);
